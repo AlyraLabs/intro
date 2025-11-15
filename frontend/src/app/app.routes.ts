ab662e71acaf1e491ecb7a7a8acdceee4449559b
kerrnney <kerrnney@gmail.com>
2025-11-15 16:14:12 +0100

    - hide terms & policy

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,8 +3,8 @@ import { IntroComponent } from './intro/intro.component';
 import { DevelopersComponent } from './developers/developers.component';
 import { CommunityComponent } from './community/community.component';
 import { MediakitComponent } from './mediakit/mediakit.component';
-import { TermsComponent } from './terms of use/terms.component';
-import { PolicyComponent } from './privacy policy/policy.component';
+// import { TermsComponent } from './terms of use/terms.component';
+// import { PolicyComponent } from './privacy policy/policy.component';
 import { HowComponent } from './how it works/how.component';
 
 export const routes: Routes = [
@@ -12,8 +12,8 @@ export const routes: Routes = [
   { path: 'developers', component: DevelopersComponent, title: 'Developers | Alyra' },
   { path: 'community', component: CommunityComponent, title: 'Community | Alyra' },
 	{ path: 'mediakit', component: MediakitComponent, title: 'Media Kit | Alyra' },
-	{ path: 'terms', component: TermsComponent, title: 'Terms of Use | Alyra' },
-	{ path: 'policy', component: PolicyComponent, title: 'Privacy Policy | Alyra' },
+	// { path: 'terms', component: TermsComponent, title: 'Terms of Use | Alyra' },
+	// { path: 'policy', component: PolicyComponent, title: 'Privacy Policy | Alyra' },
 	{ path: 'execution', component: HowComponent, title: 'How it works | Alyra' },
   { path: '**', redirectTo: '' }
 ];
